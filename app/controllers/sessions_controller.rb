d68f51358135869d8e17c97ea4c24b0d9507d809
Carl Richmond <richmond.carl@outlook.com>
2017-05-30 20:26:29 -0600

    completed authentication tests for users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,17 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.username}!"
+      redirect_to "/dashboard"
+    else
+      render :new
+    end
+  end
+
   def destroy
     session.clear
     redirect_to login_path
