6cc97ff9f5353ad85afc2758b8588d7b430f7841
Rhonda <rwilhel@gmail.com>
2017-06-01 10:17:35 -0600

    Progress made on category spec

--- spec/features/guest/guest_can_browse_items_by_category_spec.rb
+++ spec/features/guest/guest_can_browse_items_by_category_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "guest can browse categories" do
   it "can see all categories" do
-    guest = create(:guest)
 
-    visit guest_items_path
+    visit items_path
     click_on "Categories"
 
-    expect(current_path).to eq("/guest/categories")
+    expect(current_path).to eq("/categories/#{category.id}")
     expect(page).to have_content("All Categories")
     expect(page).to have_content("Potions")
     expect(page).to have_content("Bombs")
   end
 
-  it "can go to a specific category" do
+  xit "can go to a specific category" do
     guest = create(:guest)
 
     visit guest_categories_path
