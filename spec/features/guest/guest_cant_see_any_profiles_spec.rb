c251a2bae8d1da369f3c30133a6f61bb579beb44
MasonHolland <masontholland@gmail.com>
2017-06-03 15:17:44 -0600

    user story 17 passing, guests are redirected when trying to view a users show page

--- spec/features/guest/guest_cant_see_any_profiles_spec.rb
+++ spec/features/guest/guest_cant_see_any_profiles_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "guest cannot view user accounts" do
   it "when visiting a patrons page guest is redirecected" do
     link = User.create(username: "Link", password: "password")
-    lunk= User.create(username: "Lunk", password: "password")
+    lunk = User.create(username: "Lunk", password: "password")
     visit dashboard_path
 
     visit patron_user_path(link)
@@ -13,5 +13,6 @@ RSpec.describe "guest cannot view user accounts" do
     visit patron_user_path(lunk)
 
     expect(current_path).to eq("/dashboard")
+
   end
 end
