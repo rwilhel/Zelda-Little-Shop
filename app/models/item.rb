b5617e945a21a3fcb294dd29c18c418603c60eeb
Carl Richmond <richmond.carl@outlook.com>
2017-06-08 08:54:27 -0600

    work on default image for paperclip

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   has_many :orders_items
   has_many :orders, through: :orders_items
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/shield_item.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   extend FriendlyId
